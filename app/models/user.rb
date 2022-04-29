f4e374ed21b28281c2bd5068082c021cca0f813d
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-04-29 17:39:57 +0900

    モデルにバリデーション追加（編集）

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/}, confirmation: true
-  validates :last_name, presence: true, format:{/\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format:{/\A[ぁ-んァ-ン一-龥]/}
-  validates :last_name_kana, presence: true, format:{/\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format:{/\A[ァ-ヶー－]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with:/\A[ァ-ヶー－]+\z/}
   validates :birth_date, presence: true
 
 end
