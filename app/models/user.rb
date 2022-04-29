4c433f27144f562479223dfad6df5749ad97ecd9
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-04-29 17:06:10 +0900

    モデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/}, confirmation: true
+  validates :last_name, presence: true, format:{/\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format:{/\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format:{/\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format:{/\A[ァ-ヶー－]+\z/}
+  validates :birth_date, presence: true
+
 end
