cf0a9fe3c2949e17747e42d2de5dd19d166b3ef9
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-04-30 17:32:25 +0900

    モデルの単体テストコードの記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
   has_many :orders
   
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, format:{with: /\A[a-z0-9]+\z/}, confirmation: true
+  validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
