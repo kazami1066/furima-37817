f668ce1cea5c6abdf562679b77d384fcfc192a34
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-04-30 18:49:31 +0900

    該当機能実装の差分をコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
   
   validates :nickname, presence: true
   validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
