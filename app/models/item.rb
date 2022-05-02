8146c6a7c026394cbc2109fef460317b16cb6734
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-02 15:23:11 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,17 @@ class Item < ApplicationRecord
   validates :prefecture_id,          presence: true
   validates :scheduled_delivery_id,  presence: true
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  validates :category_id,            numericality: { other_than: 1 , message: "Category can't be blank"} 
+  validates :csales_status_id,       numericality: { other_than: 1 , message: "Sales status can't be blank"} 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "Shipping fee status can't be blank"} 
+  validates :prefecture_id,          numericality: { other_than: 1 , message: "Prefecture can't be blank"} 
+  validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "Scheduled delivery can't be blank"} 
+
 end
