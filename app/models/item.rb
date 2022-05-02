e246c633059c42b4a2ba5f9d2aac304222131cc4
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-02 12:53:13 +0900

    出品項目の必須化

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,16 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :order
-  
+
+  has_one_attached :image
+
+  validates :item_name,              presence: true
+  validates :item_info,              presence: true
+  validates :price,                  presence: true
+  validates :category_id,            presence: true
+  validates :sales_status_id,        presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :scheduled_delivery_id,  presence: true
+
 end
