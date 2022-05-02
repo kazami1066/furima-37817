134dec28923b890362a56b3326add90e8d5da910
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-02 10:45:16 +0900

    出品時のログイン・ログアウト状態のページ遷移変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, )
   end
 end
