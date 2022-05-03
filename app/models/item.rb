d80b0c508e6634f4d7ac7c365d493fe11d58df4d
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-03 11:52:09 +0900

    単体テストコードの記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,21 +2,24 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :order
-
   has_one_attached :image
 
   validates :item_name,              presence: true
   validates :item_info,              presence: true
-  validates :price,                  presence: true
   validates :category_id,            presence: true
   validates :sales_status_id,        presence: true
   validates :shipping_fee_status_id, presence: true
   validates :prefecture_id,          presence: true
   validates :scheduled_delivery_id,  presence: true
+  validates :image,                  presence: true
+ 
 
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_then_or_equal_to: 9_999_999 },
-                      presence: { message: "Price is invalid" }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_then_or_equal_to: 9_999_999 }
+  end
+
+  def was_attached?
+    self.image.attached?
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -26,10 +29,10 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  validates :category_id,            numericality: { other_than: 1 , message: "Category can't be blank"} 
-  validates :sales_status_id,        numericality: { other_than: 1 , message: "Sales status can't be blank"} 
-  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "Shipping fee status can't be blank"} 
-  validates :prefecture_id,          numericality: { other_than: 1 , message: "Prefecture can't be blank"} 
-  validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "Scheduled delivery can't be blank"} 
+  validates :category_id,            numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :sales_status_id,        numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id,          numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"} 
 
 end
