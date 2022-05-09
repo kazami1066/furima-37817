f43cf9c8cf1e0090a8157ecb05bb4c6c3d9a142f
kazami1066 <103098352+kazami1066@users.noreply.github.com>
2022-05-09 10:39:35 +0900

    上限金額の変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
 
   validates :item_name,              presence: true
   validates :item_info,              presence: true
-  validates :price,                  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_then_or_equal_to: 9_999_999 }
+  validates :price,                  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id,            presence: true
   validates :sales_status_id,        presence: true
   validates :shipping_fee_status_id, presence: true
